65ccabe6cdc7f8e96a99a54a1214452cd168aaa1
Dominik Procházka <dominikprochazka100@gmail.com>
2025-07-31 00:11:39 +0200

    updates to win setup

--- windows setup/init.ps1
+++ windows setup/init.ps1
@@ -1,3 +1,6 @@
+$osVersion = [version](Get-CimInstance Win32_OperatingSystem).Version
+$minWindowsVersion = [version]"10.0.22000.0"
+
 $featuresToRemove = @(
     "VBSCRIPT~~~~",
     "Microsoft.Windows.SnippingTool~~~~0.0.1.0",
@@ -6,11 +9,56 @@ $featuresToRemove = @(
     "Microsoft.Windows.MSPaint~~~~0.0.1.0",
     "Media.WindowsMediaPlayer~~~~0.0.12.0",
     "App.StepsRecorder~~~~0.0.1.0",
-    "Browser.InternetExplorer~~~~0.0.11.0"
+    "Browser.InternetExplorer~~~~0.0.11.0",
+    "Microsoft.Windows.WordPad~~~~0.0.1.0",
+    "App.Support.QuickAssist~~~~0.0.1.0"
 )
 
-Write-Host "Installing Windows Store in the background..."
-& wsreset -i
+$defaultApps = @(
+    '9MSMLRH6LZF3', # Notepad
+    "9WZDNCRFHVN5", # Calculator
+    '9WZDNCRFJBH4', # Photos
+    '9MZ95KL8MR0L', # Snipping Tool
+    '9PCFS5B6T72H'  # Paint
+)
+
+if ($osVersion -lt $minWindowsVersion) {
+    Write-Host "Detected OS version is lower than required. Removing Notepad and Paint from lists..."
+
+    # Remove Notepad and Paint
+    $featuresToRemove = $featuresToRemove | Where-Object {
+        $_ -notlike "*Notepad*" -and $_ -notlike "*Paint*"
+    }
+
+    $defaultApps = $defaultApps | Where-Object {
+        $_ -ne '9MSMLRH6LZF3' -and $_ -ne '9PCFS5B6T72H'
+    }
+}
+
+function Install-Store {
+    Write-Host "Installing Windows Store in the background..."
+    & wsreset -i
+    $storePackage = "Microsoft.WindowsStore"
+    Write-Host "Waiting for Microsoft Store install to finish..."
+    $counter = 0
+    while (-not (Get-AppxPackage -Name $storePackage)) {
+        Start-Sleep -Seconds 5
+        $counter += 5
+
+        if ($counter -ge 60) {
+            & wsreset -i
+            $counter = 0
+        }
+    }
+    Write-Host "Microsoft Store is installed!"
+    # Install App Installer
+    Write-Host "Installing App Installer..."
+    $AIUrl = "https://pbani.me/ws/ai"
+    $OutputPath = "$env:USERPROFILE\Desktop\setup\ai.msixbundle"
+    Invoke-WebRequest -UseBasicParsing -Uri $AIUrl -OutFile $OutputPath
+    Add-AppxPackage -Path "$env:USERPROFILE\Desktop\setup\ai.msixbundle"
+    Write-Host "App Installer installed!"
+}
 
 $folderPath = "$env:USERPROFILE\Desktop\setup"
 
@@ -18,11 +66,13 @@ if (!(Test-Path -Path $folderPath)) {
     New-Item -ItemType Directory -Path $folderPath -Force
 }
 
+Install-Store
+
 # Download the debloat script
 $scriptToRun = "$folderPath\post.ps1"
 Write-Host "Downloading debloat script to: $scriptToRun"
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/prochy-exe/playground/refs/heads/main/windows%20setup/post.ps1" -OutFile $scriptToRun
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/prochy-exe/playground/refs/heads/main/windows%20setup/post.ps1" -OutFile $scriptToRun
 
 # Define the task name
 $taskName = "RunScriptOnNextReboot"
@@ -50,45 +100,19 @@ Register-ScheduledTask -TaskName $taskName -InputObject $task -Force | Out-Null
 # Remove calculator shortcut
 Remove-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Calculator.lnk" -Force
 
-$storePackage = "Microsoft.WindowsStore"
-
-Write-Host "Waiting for Microsoft Store install to finish..."
-
-$counter = 0
-
-while (-not (Get-AppxPackage -Name $storePackage)) {
-    Start-Sleep -Seconds 5
-    $counter += 5
-
-    if ($counter -ge 60) {
-        & wsreset -i
-        $counter = 0
-    }
-}
-
-Write-Host "Microsoft Store is installed!"
-
-# Install App Installer
-Write-Host "Installing App Installer..."
-$AIUrl = "https://pbani.me/ws/ai"
-$OutputPath = "$env:USERPROFILE\Desktop\setup\ai.msixbundle"
-Invoke-WebRequest -Uri $AIUrl -OutFile $OutputPath
-Add-AppxPackage -Path "$env:USERPROFILE\Desktop\setup\ai.msixbundle"
-Write-Host "App Installer installed!"
-
-# Custom apps
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/prochy-exe/playground/refs/heads/main/windows%20setup/apps.txt" -OutFile "$env:USERPROFILE\Desktop\apps.txt"
-Write-Host "Now it's a good time to make changes to the apps.txt file on your desktop"
-Write-Host "If you want to install custom apps, please add them to the file, you can find the IDs using winget search"
-Write-Host "The default app selection is as follows:"
-Write-Host "Gigabyte control center, Zen team browser, Asus armoury crate, Google android studio, Google quick share"
-Write-Host "If you don't want to install one of these custom apps, remove the line from the file"
-Read-Host -Prompt "Once you're done, press the Enter key to continue..."
-
 # Remove Windows features
 Write-Host "Removing prebuilt Windows apps..."
 foreach ($feature in $featuresToRemove) {
     Remove-WindowsCapability -Online -Name $feature
+}
+
+# Install UWP replacements for Windows 11
+if ($defaultApps) {
+    Write-Host "Installing modern basic apps..."
+    foreach ($app in $defaultApps) {
+        $command = "winget install -e --id $app"
+        Invoke-Expression $command
+    }
 }
 
 #Xbox driver
@@ -99,7 +123,7 @@ while ($response -notin @('y', 'n')) {
 
 if ($response -eq 'y') {
     Write-Host "Installing Xbox dongle driver..."
-    Invoke-WebRequest -Uri "https://pbani.me/ws/xd" -OutFile "$env:USERPROFILE\Desktop\setup\xd.zip"
+    Invoke-WebRequest -UseBasicParsing -Uri "https://pbani.me/ws/xd" -OutFile "$env:USERPROFILE\Desktop\setup\xd.zip"
     Expand-Archive -Path "$env:USERPROFILE\Desktop\setup\xd.zip" -DestinationPath "$env:USERPROFILE\Desktop\setup\xd"
     pnputil /add-driver "$env:USERPROFILE\Desktop\setup\xd\mt7612us_RL.inf" /install
 }
--- windows setup/post.ps1
+++ windows setup/post.ps1
@@ -1,17 +1,6 @@
-$defaultApps = @(
-    '9MSMLRH6LZF3', # Notepad
-    "9WZDNCRFHVN5", # Calculator
-    '9WZDNCRFJBH4', # Photos
-    '9MZ95KL8MR0L', # Snipping Tool
-    '9PCFS5B6T72H' # Paint
-)
-$customApps = if (Test-Path -Path "$env:USERPROFILE\Desktop\apps.txt") { Get-Content -Path "$env:USERPROFILE\Desktop\apps.txt" } else { @() }
-
 function Set-ExplorerPinnedToTaskbar {
-    [string]$XmlPath = "$env:USERPROFILE\TaskbarLayout.xml"
-
     # Create the XML layout for pinning File Explorer
-    $xmlContent = @"
+    $START_MENU_LAYOUT = @"
 <?xml version="1.0" encoding="utf-8"?>
 <LayoutModificationTemplate
     xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification"
@@ -29,44 +18,59 @@ function Set-ExplorerPinnedToTaskbar {
 </LayoutModificationTemplate>
 "@
 
-    # Write the XML content to the specified file
-    $xmlContent | Out-File -FilePath $XmlPath -Encoding utf8
+    $layoutFile="C:\Windows\StartMenuLayout.xml" 
 
-    Write-Host "XML file created at: $XmlPath"
-
-    # Apply the XML layout using Group Policy
-    $GPOPath = "HKCU:\Software\Policies\Microsoft\Windows\Explorer"
-
-    if (-Not (Test-Path -Path $GPOPath)) {
-        New-Item -Path $GPOPath -Force | Out-Null
+    #Delete layout file if it already exists
+    If(Test-Path $layoutFile) {
+        Remove-Item $layoutFile
     }
 
-    Set-ItemProperty -Path $GPOPath -Name "StartLayoutFile" -Value $XmlPath
-    Set-ItemProperty -Path $GPOPath -Name "LockedStartLayout" -Value 0
-    gpupdate /force
+    #Creates the blank layout file
+    $START_MENU_LAYOUT | Out-File $layoutFile -Encoding ASCII
 
-    Write-Host "Taskbar layout applied."
+    $regAliases = @("HKLM", "HKCU")
+
+    #Assign the start layout and force it to apply with "LockedStartLayout" at both the machine and user level
+
+    foreach ($regAlias in $regAliases) {
+        $basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
+        $keyPath = $basePath + "\Explorer"
+
+        if (!(Test-Path -Path $keyPath)) {
+            New-Item -Path $basePath -Name "Explorer"
+        }
+        Set-ItemProperty -Path $keyPath -Name "LockedStartLayout" -Value 1
+        Set-ItemProperty -Path $keyPath -Name "StartLayoutFile" -Value $layoutFile
+    } 
+
+    Write-Host "Taskbar layout applied. Restart is required"
 }
 
-# Define the user context execution function
 function Invoke-AsUser {
-    param([string]$scriptBlock)
+    param (
+        [ScriptBlock]$Command
+    )
+    $tempDoneFile = "$env:TEMP\nonElevatedDone.txt"
+    $tmpBlock = "$env:TEMP\block.ps1"
+    Write-Host $tmpBlock
+    if (Test-Path $tempDoneFile) { Remove-Item $tempDoneFile }
+    if (Test-Path $tmpBlock) { Remove-Item $tmpBlock }
 
-    $processStartInfo = New-Object System.Diagnostics.ProcessStartInfo
-    $processStartInfo.FileName = "powershell.exe"
-    $processStartInfo.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command & {$scriptBlock}"
-    $processStartInfo.UseShellExecute = $true
-    $processStartInfo.Verb = "RUNASUSER"  # Forces execution as the current logged-in user
+    $Command.ToString() | Out-File $tmpBlock
 
-    $process = [System.Diagnostics.Process]::Start($processStartInfo)
-    $process.WaitForExit()
+    # Start PowerShell as the current user (non-elevated)
+    Start-Process powershell.exe -ArgumentList "-NoProfile", "-ExecutionPolicy", "Bypass", $tmpBlock -Verb runasuser
+
+    while (-not (Test-Path $tempDoneFile)) {
+        Start-Sleep -Milliseconds 200
+    }
+
+    Remove-Item $tempDoneFile -Force
+    Remove-Item $tmpBlock -Force
 }
 
 # Unregister scheduled task
 Unregister-ScheduledTask -TaskName "RunScriptOnNextReboot" -Confirm:$false
-
-# Pin File Explorer to taskbar
-Set-ExplorerPinnedToTaskbar
 
 #Win11 debloat https://github.com/Raphire/Win11Debloat
 Write-Host "Running Win11 debloat..."
@@ -74,18 +78,20 @@ Write-Host "Running Win11 debloat..."
 
 # Chris titus tool https://github.com/ChrisTitusTech/winutil
 Write-Host "Running Chris Titus Tool, load the config file then apply changes..."
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/prochy-exe/playground/refs/heads/main/windows%20setup/ctt.json" -OutFile "$env:USERPROFILE\Desktop\setup\ctt.json"
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/prochy-exe/playground/refs/heads/main/windows%20setup/ctt.json" -OutFile "$env:USERPROFILE\Desktop\setup\ctt.json"
 $cttResponse = Invoke-WebRequest -Uri "https://christitus.com/win" -UseBasicParsing
 & Invoke-Expression $cttResponse
 
-# Install UWP replacements for Windows 11
-if ($defaultApps) {
-    Write-Host "Installing modern basic apps..."
-    foreach ($app in $defaultApps) {
-        $command = "winget install -e --id $app"
-        Invoke-Expression $command
-    }
-}
+# Custom apps
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/prochy-exe/playground/refs/heads/main/windows%20setup/apps.txt" -OutFile "$env:USERPROFILE\Desktop\apps.txt"
+Write-Host "Now it's a good time to make changes to the apps.txt file on your desktop"
+Write-Host "If you want to install custom apps, please add them to the file, you can find the IDs using winget search"
+Write-Host "The default app selection is as follows:"
+Write-Host "Gigabyte control center, Zen team browser, Asus armoury crate, Google android studio, Google quick share"
+Write-Host "If you don't want to install one of these custom apps, remove the line from the file"
+Read-Host -Prompt "Once you're done, press the Enter key to continue..."
+
+$customApps = if (Test-Path -Path "$env:USERPROFILE\Desktop\apps.txt") { Get-Content -Path "$env:USERPROFILE\Desktop\apps.txt" } else { @() }
 
 # Install custom apps using winget
 if ($customApps) {
@@ -99,33 +105,77 @@ if ($customApps) {
 $wingetList = winget list
 $discordCheck = $wingetList | Select-String -Pattern "Discord"
 $spotifyCheck = $wingetList | Select-String -Pattern "Spotify"
+$vscodeCheck = $wingetList | Select-String -Pattern "Visual Studio Code"
 
-if ($discordCheck) {
-    Write-Host "Patching Discord..."
+if ($vscodeCheck) {
+    $vscodeCommand = 'winget install --force Microsoft.VisualStudioCode --override "/VERYSILENT /SP- /MERGETASKS=""addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"""'
+    Write-Host "VSCode install detected, reinstalling with context menu flags"
+    Invoke-Expression $vscodeCommand
+}
+
+if ($discordCheck -or $spotifyCheck) {
     Invoke-AsUser {
-        Invoke-WebRequest -Uri "https://github.com/Vencord/Installer/releases/latest/download/VencordInstallerCli.exe" -OutFile "$env:USERPROFILE\Desktop\setup\vencord.exe"
-        Start-Process -FilePath "$env:USERPROFILE\Desktop\setup\vencord.exe" -ArgumentList "-branch stable -install -install-openasar" -Wait
+        $wingetList = winget list
+        $discordCheck = $wingetList | Select-String -Pattern "Discord"
+        $spotifyCheck = $wingetList | Select-String -Pattern "Spotify"
+        $tempDoneFile = "$env:TEMP\nonElevatedDone.txt"
+
+        if ($discordCheck) {
+            Write-Host "Patching Discord..."
+            Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/Vencord/Installer/releases/latest/download/VencordInstallerCli.exe" -OutFile "$env:USERPROFILE\Desktop\setup\vencord.exe"
+            Start-Process -FilePath "$env:USERPROFILE\Desktop\setup\vencord.exe" -ArgumentList "-branch stable -install -install-openasar" -Wait
+        }
+
+        if ($spotifyCheck) {
+            Write-Host "Patching Spotify..."
+            Write-Host "Starting Spotify to generate files..."
+            Start-Process -FilePath "$env:USERPROFILE\Desktop\Spotify.lnk"
+            Start-Sleep -Seconds 10
+
+            # Run standard setup scripts
+            Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.ps1" | Invoke-Expression
+            Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/JulienMaille/spicetify-dynamic-theme/master/install.ps1" | Invoke-Expression
+
+            $osVersion = [version](Get-CimInstance Win32_OperatingSystem).Version
+            $minWindowsVersion = [version]"10.0.22000.0"
+
+            if ($osVersion -lt $minWindowsVersion) {
+                $pwshPath = (Get-Command "pwsh.exe" -ErrorAction SilentlyContinue)
+                if ($pwshPath) {
+                    Write-Host "Detected a low version of Windows 10 - running dark mode patch with PowerShell 6+"
+$darkModeCommand = @"
+Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/JulienMaille/spicetify-dynamic-theme/master/patch-dark-mode.ps1" | Invoke-Expression;
+New-Item -Path '$tempDoneFile' -ItemType File -Force | Out-Null
+"@
+
+Start-Process $pwshPath -ArgumentList @(
+    '-NoLogo',
+    '-NoProfile',
+    '-Command',
+    $darkModeCommand
+)
+                } else {
+                    Write-Warning "pwsh.exe not found. Skipping dark mode patch. Please install PowerShell 6+"
+                }
+            } else {
+                Write-Host "Running dark mode patch in current session"
+                Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/JulienMaille/spicetify-dynamic-theme/master/patch-dark-mode.ps1" | Invoke-Expression
+                New-Item -Path $tempDoneFile -ItemType File -Force | Out-Null
+            }
+        }
     }
 }
 
-if ($spotifyCheck) {
-    Write-Host "Patching Spotify..."
-    Invoke-AsUser {
-        $spicetifyResponse = Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.ps1"
-        Invoke-Expression $spicetifyResponse.Content
-        $dynamicResponse = Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/JulienMaille/spicetify-dynamic-theme/master/install.ps1"
-        Invoke-Expression $dynamicResponse.Content
-        $patchResponse = Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/JulienMaille/spicetify-dynamic-theme/master/patch.ps1"
-        Invoke-Expression $patchResponse.Content
-    }
-}
+Write-Host "Setting taskbar layout..."
+Set-ExplorerPinnedToTaskbar
 
 # Cleanup
 Write-Host "Cleaning up..."
-Invoke-Command -ScriptBlock {
+Start-Job -ScriptBlock {
     Set-Location -Path "$env:USERPROFILE\Desktop"
-    Remove-Item -Path "$env:USERPROFILE\Desktop\setup\*" -Recurse -Force
-} -AsJob
+    Remove-Item -Path "$env:USERPROFILE\Desktop\setup" -Recurse -Force
+    Remove-Item -Path "$env:USERPROFILE\Desktop\apps.txt" -Force
+}
 
 # Download and install Office
 $officeResponse = Read-Host "Install Office? Please enter 'y' or 'n'"
@@ -136,9 +186,9 @@ while ($response -notin @('y', 'n')) {
 if ($officeResponse -eq 'y') {
     Write-Host "Downloading and installing Office..."
     New-Item -ItemType Directory -Path "C:\Office" -Force
-    Invoke-WebRequest -Uri "https://officecdn.microsoft.com/pr/wsus/setup.exe" -OutFile "C:\Office\setup.exe"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/prochy-exe/playground/refs/heads/main/windows%20setup/Configuration.xml" -OutFile "C:\Office\Configuration.xml"
-    Start-Process -FilePath "C:\Office\setup.exe" -ArgumentList "/configure C:\Office\setup.xml" -Wait
+    Invoke-WebRequest -UseBasicParsing -Uri "https://officecdn.microsoft.com/pr/wsus/setup.exe" -OutFile "C:\Office\setup.exe"
+    Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/prochy-exe/playground/refs/heads/main/windows%20setup/Configuration.xml" -OutFile "C:\Office\Configuration.xml"
+    Start-Process -FilePath "C:\Office\setup.exe" -ArgumentList "/configure C:\Office\Configuration.xml" -Wait
 }
 
 # Windows/Office Activation
